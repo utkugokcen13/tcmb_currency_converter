0bb026e31f585d807143f6114fafd55fbf9db10c
Utku Gökçen <utku.gokcen13@gmail.com>
2021-02-27 11:42:23 +0300

    effective dollar price taken

--- convert_currency.py
+++ convert_currency.py
@@ -1,1 +1,17 @@
-print("Hello dollar/euro")
+# Effective dollar value will be taken from the TCMB website.
+
+# https://www.turkiye.gov.tr/doviz-kurlari
+
+from bs4 import *
+import requests
+
+site = requests.get("https://www.turkiye.gov.tr/doviz-kurlari")
+
+soup = BeautifulSoup(site.text, "html.parser")
+
+efektif_dolar_satis = soup.find('td', text='1 ABD DOLARI').find_next_sibling('td')\
+                                                          .find_next_sibling('td')\
+                                                          .find_next_sibling('td')\
+                                                          .find_next_sibling('td').text
+
+print(efektif_dolar_satis)
