bf998dfdd621ef380990843af752ddf33f992036
Utku Gökçen <utku.gokcen13@gmail.com>
2021-02-27 12:18:14 +0300

    bug fixed

--- convert_currency.py
+++ convert_currency.py
@@ -12,6 +12,6 @@ soup = BeautifulSoup(site.text, "html.parser")
 efektif_dolar_satis = soup.find('td', text='1 ABD DOLARI').find_next_sibling('td')\
                                                           .find_next_sibling('td')\
                                                           .find_next_sibling('td')\
-                                                          .text
+                                                          .find_next_sibling('td').text
 
 print(efektif_dolar_satis)
